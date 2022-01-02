3c22d07aeb64ae9cae3f024930a52bebf82e3e4c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-02 18:37:38 +0700

    feat: add example extension function from class Car

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -14,8 +14,16 @@ class Car {
   Car({required this.type, required this.price});
 }
 
+// Extension function from class Car
+extension CarAction on Car {
+  void run() {
+    print("Car type: $type and price: Rp$price");
+  }
+}
+
 void main(List<String> arguments) {
   Student student = Student("Alfian", "891");
 
   Car car = Car(type: "Sedan", price: 10000000.0);
+  car.run(); // Using method run even not available on class Car
 }
