cf0af269e431a2d29ac00cac7ab54173f202a2ae
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-02 18:33:10 +0700

    feat: add example class with named parameter

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -6,6 +6,16 @@ class Student {
   Student(this.name, this.id);
 }
 
+// Class constructor with named parameter
+class Car {
+  String type;
+  double price;
+
+  Car({required this.type, required this.price});
+}
+
 void main(List<String> arguments) {
   Student student = Student("Alfian", "891");
+
+  Car car = Car(type: "Sedan", price: 10000000.0);
 }
