19c42aea88452ccb3e7a4fd95ed8cdfff3442741
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-02 18:51:20 +0700

    feat: add example of Redirecting class

--- bin/dart_oop.dart
+++ bin/dart_oop.dart
@@ -21,9 +21,26 @@ extension CarAction on Car {
   }
 }
 
+// Redirecting Class
+enum PersonGender { male, female }
+
+class Person {
+  String name;
+  PersonGender gender;
+
+  Person({required this.name, required this.gender});
+
+  Person.male({required String name}): this(name: name, gender: PersonGender.male);
+
+  Person.female({required String name}): this(name: name, gender: PersonGender.female);
+}
+
 void main(List<String> arguments) {
   Student student = Student("Alfian", "891");
 
   Car car = Car(type: "Sedan", price: 10000000.0);
   car.run(); // Using method run even not available on class Car
+
+  Person andy = Person.male(name: "Alfian");
+  Person novy = Person.female(name: "Novi");
 }
